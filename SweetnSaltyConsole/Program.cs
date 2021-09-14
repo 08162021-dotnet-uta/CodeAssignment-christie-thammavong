ec81df3eaf8eb98f7d08ed7063201149de034c87
christie thammavong <christie.thammavong@revature.net>
2021-09-14 09:00:02 -0600

    current progress

--- SweetnSaltyConsole/Program.cs
+++ SweetnSaltyConsole/Program.cs
@@ -6,7 +6,34 @@ namespace SweetnSaltyConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          int sweet = 0;
+          int salty = 0;
+          int sweetnSalty = 0;
+          for (int i = 1; i < 1001; i++)
+          {
+            if ((i % 3) == 0 && (i % 5) == 0)
+            {
+              Console.Write("sweet'nSalty ");
+              sweetnSalty++;
+            }
+            else if ((i % 3) == 0)
+            {
+              Console.Write("sweet ");
+              sweet++;
+            }
+            else if ((i % 5) == 0)
+            {
+              Console.Write("salty ");
+              salty++;
+            }
+            else
+            {
+              Console.Write(i + " ");
+            }
+          }
+          Console.WriteLine("Sweet: " + sweet);
+          Console.WriteLine("Salty: " + salty);
+          Console.WriteLine("Sweet'nSalty: " + sweetnSalty);
         }
     }
 }
